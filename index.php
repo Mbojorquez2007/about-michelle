667de8ab968f225c5b2610f7b0b7658586651251
Michelle <mbojorquez4@cnm.edu>
2017-07-11 14:57:47 -0600

    about michelle page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>about michelle</title>
+	</head>
+	<body>
+		<p> Hello, my name is Michelle I grow up in Albuquerque, I like New Mexico its very beautiful. I have like to cook and to hang out with my family. I like computers and I am every excited to learn more about them
+		</p> I have four beautiful child. They are the loves of my life.I have a 14 year daughter, a 10 year old daughter, a 9 year daughter, and a 2 year son.
+
+		<p>I have done retail and looking forward to my new opportunity   </p>
+		<img src="image/michelle.jpg"/>
+	</body>
